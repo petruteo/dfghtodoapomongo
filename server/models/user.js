66005ef3c22e49cabd03e7895ca74618e23db340
Petru <escu.petru@gmail.com>
2018-02-13 23:28:51 +0200

    nada

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,7 @@ UserSchema.methods.generateAuthToken = function () {
 	var token = jwt.sign( {
 			_id: user._id.toHexString(),
 			access
-		}, 'abc123' )
+		}, process.env.JWT_SECRET )
 		.toString();
 
 	user.tokens.push( {
@@ -100,7 +100,7 @@ UserSchema.statics.findByToken = function ( token ) {
 	var decoded;
 
 	try {
-		decoded = jwt.verify( token, 'abc123' );
+		decoded = jwt.verify( token, process.env.JWT_SECRET );
 	} catch ( e ) {
 		// return new Promise( ( resolve, reject ) => {
 		// 	reject();
